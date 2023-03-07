c51524478560306ac46d88733d8c50299a15ac96
liuhongbo <916196375@qq.com>
2023-03-07 22:09:42 +0800

    feat: init project

--- .umirc.ts
+++ .umirc.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-03-07 21:06:41
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-03-07 21:28:21
+ * @FilePath: \MINIBBS_REACT\.umirc.ts
+ * @Description: 
+ */
 import { defineConfig } from 'umi';
 
 export default defineConfig({
@@ -8,4 +16,5 @@ export default defineConfig({
     { path: '/', component: '@/pages/index' },
   ],
   fastRefresh: {},
+  webpack5:{}
 });
